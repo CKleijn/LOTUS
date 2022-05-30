485ee7c52bece5e62457157d247e121f768af3cd
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-05-30 10:26:42 +0200

    Make seperate dbconnection file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const sessions = require('express-session');
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sessions = require("express-session");
 const homeRoutes = require(__dirname + "/src/routes/home.routes");
 const userRoutes = require(__dirname + "/src/routes/user.routes");
 
@@ -8,14 +12,16 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(sessions({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 },
-    resave: false
-}));
+app.use(
+    sessions({
+        secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+        saveUninitialized: true,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 },
+        resave: false,
+    })
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
