a055d2fc55a3117a8b488366b82d6904eb17b0f8
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-06-01 12:36:39 +0200

    Add create member functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("./../models/user.model");
 const bcrypt = require("bcrypt");
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr(process.env.EMAIL_SETUP_HASH);
 
 exports.isLoggedIn = (req, res, next) => {
     const session = req.session;
@@ -29,7 +31,8 @@ exports.login = (req, res) => {
                     }
 
                     if (firstLogin === true) {
-                        return res.redirect("/member_setup");
+                        const encryptedEmail = cryptr.encrypt(emailAddress);
+                        res.redirect(`/member_setup?t=${encryptedEmail}`);
                     } else {
                         User.findOneAndUpdate({ _id: user._id }, { lastLoginDate: Date.now() }, { new: true }, (err, updatedUser) => {
                             if (err) throw err;
@@ -52,6 +55,45 @@ exports.login = (req, res) => {
     }
 };
 
+exports.setupMember = (req, res) => {
+    const { firstName, lastName, password, email } = req.body;
+    const decryptedMail = cryptr.decrypt(email);
+
+    const errors = {};
+    const oldValues = {};
+    errors.oldValues = oldValues;
+
+    if (!firstName || firstName.length === 0) {
+        errors.firstNameErr = "Voornaam is verplicht!";
+    } else {
+        oldValues.firstName = firstName;
+    }
+
+    if (!lastName || lastName.length === 0) {
+        errors.lastNameErr = "Achternaam is verplicht!";
+    } else {
+        oldValues.lastName = lastName;
+    }
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+    if (!password || lastName.password === 0) {
+        errors.passwordErr = "Wachtwoord is verplicht!";
+    } else if (!passwordRegex.test(password)) {
+        errors.passwordErr = "Wachtwoord is niet sterk genoeg!";
+    } else {
+        oldValues.password = password;
+    }
+
+    if (typeof errors.firstNameErr != "undefined" || typeof errors.lastNameErr != "undefined" || typeof errors.passwordErr != "undefined") {
+        res.render("member_setup", { pageName: "Accountgegevens", ...errors, email });
+    } else {
+        User.findOneAndUpdate({ emailAddress: decryptedMail }, { firstName, lastName, password: bcrypt.hashSync(password, bcrypt.genSaltSync()), lastLoginDate: Date.now() }, { new: true }, (err, updatedUser) => {
+            res.redirect("/login");
+        });
+    }
+};
+
 exports.logout = (req, res) => {
     req.session.destroy();
     return res.redirect("/login");
@@ -66,5 +108,5 @@ exports.getLoginPage = (req, res) => {
 };
 
 exports.getSetupPage = (req, res) => {
-    res.render("member_setup", { pageName: "Accountgegevens instellen" });
+    res.render("member_setup", { pageName: "Accountgegevens", errors: {}, email: req.query.t });
 };
--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -4,6 +4,10 @@ exports.getHomepage = (req, res) => {
     res.render("dashboard", { pageName: "Dashboard", roles: session.roles, firstName: session.firstname });
 };
 
+exports.getUserOverview = (req, res) => {
+    res.render("user_overview", { pageName: "Gebruikers" });
+};
+
 exports.sendMessage = (req, res) => {
-    res.send("Clicked on " + req.originalUrl.replace("/", "") + ".")
-}
+    res.send("Clicked on " + req.originalUrl.replace("/", "") + ".");
+};
