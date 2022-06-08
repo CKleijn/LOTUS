9f7a7a972b86fb5648896a706a433b8aaf662660
Calvin <cl.kleijn@student.avans.nl>
2022-06-08 10:31:05 +0200

    Create request functionality for assignment

--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -1,4 +1,17 @@
 const mongoose = require("../../database/dbconnection");
+const Request = require("../models/request.model");
+
+exports.createRequest = (req, res, objectId) => {
+    // Get session
+    const session = req.session;
+    // Create request
+    const request = new Request({
+        userId: session.user.userId,
+        assignmentId: objectId.toString(),
+    });
+    // Save request
+    request.save();
+};
 
 exports.getAllRequests = (req, res) => {
     res.render("request_overview", { pageName: "Verzoeken", session: req.session.user });
